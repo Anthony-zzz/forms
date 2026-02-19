55d1a6a5a9cc8030c80a3531030662832c791228
Anthony <anthony.matheusfrz@gmail.com>
2026-02-19 16:50:20 -0300

    forms-att

--- components/home/QuickLinks.tsx
+++ components/home/QuickLinks.tsx
@@ -1,23 +1,56 @@
 import { useSession } from "@/providers/SessionContext";
 import { useTheme } from "@/themes/ThemeContext";
-import { View } from "react-native";
+import { Alert, View } from "react-native";
 import { Button } from "../Button";
+import { useRouter } from "expo-router";
+import { useState } from "react";
+import formsService from "@/services/forms-service";
 
 export const QuickLinks = () => {
   const { theme, switchTheme } = useTheme();
-  const { signOut } = useSession();
+  const { signOut, user } = useSession();
+  const router = useRouter();
+
+  const [loading, setLoading] = useState(false);
+
+  const handleCreateFrom = async () => {
+    setLoading(true);
+    if (!user) {
+      router.replace("/");
+      return;
+    }
+
+    const form = await formsService.createEmpytForm(user.id);
+    if (!form) {
+      Alert.alert("Error", "Erro");
+      return;
+    }
+  };
 
   return (
     <View style={{ gap: theme.spacing.sm }}>
-      <Button title="Criar novo formulário" onPress={() => {}} />
-      <Button title="Todos os formulários" variant="outline" onPress={() => {}} />
+      <Button
+        title="Criar novo formulário"
+        disabled={loading}
+        loading={loading}
+        onPress={() => handleCreateFrom()}
+      />
+      <Button
+        title="Todos os formulários"
+        variant="outline"
+        onPress={() => {}}
+      />
       <Button title="Ver perfil" variant="outline" onPress={() => {}} />
       <Button
         title="Alternar tema"
         variant="outline"
         onPress={() => switchTheme()}
       />
-      <Button title="Sair da conta" variant="danger" onPress={() => signOut()} />
+      <Button
+        title="Sair da conta"
+        variant="danger"
+        onPress={() => signOut()}
+      />
     </View>
   );
 };
---
+++ services/forms-service.ts
@@ -0,0 +1,42 @@
+import { supabase } from "@/lib/supabase";
+import { Alert } from "react-native";
+
+export type Formulario = {
+  id: string;
+  userId: string;
+  title: string;
+  description: string;
+  isPublished: boolean;
+};
+
+const formsService = {
+  createEmpytForm: async (userId: string) => {
+    const { data, error } = await supabase
+      .from("forms")
+      .insert({
+        user_id: userId,
+        title: "Novo formulário em branco",
+        description: "Este é um formulário vazio. Edite a descrição dele.",
+        is_published: false,
+      })
+      .select(
+        `
+        id,
+        userId: user_id,
+        title,
+        description,
+        isPublished: is_published
+      `,
+      )
+      .single<Formulario>();
+
+    if (error) {
+      console.log(error);
+      Alert.alert("Erro", "Não foi possível criar um novo formulário vazio.");
+    }
+
+    return data;
+  },
+};
+
+export default formsService;
